aec442041c8494d1644d1ec60c2d6cfe3b0e73ee
prince.titus.codjoe <prince.titus.codjoe@gmail.com>
2021-01-21 16:41:57 -0500

    fix warnings

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Menu from './components/Menu';
--- src/components/University.js
+++ src/components/University.js
@@ -10,23 +10,23 @@ class University extends Component {
         className="atbd_single_listing_wrapper directorist-featured-listings">
         <figure className="atbd_listing_thumbnail_area">
            <div className="atbd_listing_image">
-              <a href={this.props.university.web_pages[0]} target="_blank">
+              <a href={this.props.university.web_pages[0]} rel="noreferrer" target="_blank">
                  <div className='atbd-thumbnail-card card-cover' style={{height: "250px", backgroundColor: "lightyellow"}}>
                     <div className='atbd-thumbnail-card-front-wrap'><img src={'https://logo.clearbit.com/'+(this.props.university.domains[0])+"?size=250"} alt={this.props.university.name} className='atbd-thumbnail-card-front-img'/></div>
                  </div>
               </a>
               <div className="atbd_author">
                  <img src="https://demo.directorist.com/theme/direo/wp-content/uploads/2020/06/auth5-150x150.jpg"
-                    alt="Author Image" />
+                    alt="Author" />
               </div>
            </div>
 
-           <div className="atbdp_add_to_fav_listings"><a target="_blank" className="atbdp_mark_as_fav " id="atbdp-fav_2855" data-listing_id="2855" href={this.props.university.web_pages[0]}><span className="atbd_fav_icon"></span><span className="atbd_fav_tooltip"></span></a></div>
+           <div className="atbdp_add_to_fav_listings"><a rel="noreferrer" target="_blank" className="atbdp_mark_as_fav " id="atbdp-fav_2855" data-listing_id="2855" href={this.props.university.web_pages[0]}><span className="atbd_fav_icon"></span><span className="atbd_fav_tooltip"></span></a></div>
         </figure>
         <div className="atbd_listing_info">
            <div className="atbd_content_upper">
               <h4 className="atbd_listing_title">
-                 <a href={this.props.university.web_pages[0]} target="_blank">{this.props.university.name}</a>
+                 <a href={this.props.university.web_pages[0]} rel="noreferrer" target="_blank">{this.props.university.name}</a>
               </h4>
            </div>
         </div>
